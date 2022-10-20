a2afd39018c00b0d3df138f3c6acbed87fccb3ac
Fuzzkatt <zonghan2000@gmail.com>
2022-10-20 14:02:11 -0700

    convert pngs back to single pdf

--- pdf2filteredpngs.py
+++ pdf2filteredpngs.py
@@ -2,13 +2,20 @@ import requests
 import json
 import argparse
 import fitz
+from PIL import Image
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pdf_file')
 args = parser.parse_args()
 
 doc = fitz.open(args.pdf_file)
+
+images = []
+new_pdf_file = f"{args.pdf_file[:-4]}_fixed.pdf"
+
 for i in range(len(doc)):
+    print(f"working on page {i+1} of {len(doc)}...")
     page = doc[i]
     pix = page.get_pixmap()
     png_file = f"{args.pdf_file[:-4]}_page{i+1}.png"
@@ -31,3 +38,9 @@ for i in range(len(doc)):
     img_data = requests.get(url).content
     with open(png_file, 'wb') as handler:
         handler.write(img_data)
+
+    png = Image.open(png_file).convert("RGB")
+    images.append(png)
+
+images[0].convert("RGB").save(new_pdf_file, resolution=100.0, save_all=True, append_images=images[1:])
+
