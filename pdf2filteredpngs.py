2e871d860e508fa5752f8cccdd7abb33f7633ded
Fuzzkatt <zonghan2000@gmail.com>
2022-10-26 16:34:57 -0700

    finalized pipeline

--- pdf2filteredpngs.py
+++ pdf2filteredpngs.py
@@ -3,11 +3,11 @@ import json
 import argparse
 import fitz
 from PIL import Image
-
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pdf_file')
-parser.add_argument('filter')
+parser.add_argument('--adjust', default=False, action="store_true")
 args = parser.parse_args()
 
 doc = fitz.open(args.pdf_file)
@@ -15,43 +15,49 @@ doc = fitz.open(args.pdf_file)
 images = []
 new_pdf_file = f"{args.pdf_file[:-4]}_fixed.pdf"
 
-for i in range(len(doc)):
+# for each page in the pdf
+for i in range(2, 3):
+# for i in range(len(doc)):
     print(f"working on page {i+1} of {len(doc)}...")
+    
+    # store page as temporary png files
     page = doc[i]
-    pix = page.get_pixmap(dpi=244)
+    pix = page.get_pixmap(dpi=288)
     png_file = f"{args.pdf_file[:-4]}_page{i+1}.png"
     pix.save(png_file)
 
-    if args.filter == 'upscale':
-        url = "https://api.picsart.io/tools/demo/upscale/enhance"
-        payload={"unit": "px", "format": "JPG", "upscale_factor": "2"}
-    elif args.filter == 'removebg':
-        url = "https://api.picsart.io/tools/demo/removebg"
-        payload={"bg_blur": "0", "scale": "fit", "format": "PNG", "output_type": "cutout"}
-    elif args.filter == 'vectorizer':
-        url = "https://api.picsart.io/tools/demo/vectorizer"
-        payload={"downscale_to": "2048"}
-    elif args.filter == 'adjust':
-        url = "https://api.picsart.io/tools/demo/adjust"
-        payload={"contrast": "0", "brightness": "0", "saturation": "0", "sharpen": "0", "shadows": "0", "clarity": "0", "temperature": "0", "format": "JPG", "hue": "0", "vignette": "0", "noise": "0", "highlights": "0"}
-
-    files=[
-        ('image',(png_file, open(png_file, 'rb'),'image/png'))
-    ]
-    headers = {
-        "accept": "application/json", "apikey": "sifH0Y0MoRLcboeYh899RiWFw29vt0Pz"
-    }
-
+    # do mandatory upscale
+    url = "https://api.picsart.io/tools/demo/upscale/enhance"
+    payload={"unit": "px", "format": "PNG", "upscale_factor": "2"}
+    files=[('image',(png_file, open(png_file, 'rb'),'image/png'))]
+    headers = {"accept": "application/json", "apikey": "sifH0Y0MoRLcboeYh899RiWFw29vt0Pz"}
     response = requests.request("POST", url, headers=headers, data=payload, files=files)
     data = json.loads(response.text)
     url = data['data']['url']
-
     img_data = requests.get(url).content
     with open(png_file, 'wb') as handler:
         handler.write(img_data)
 
+    # do optional adjusts
+    if args.adjust:
+        url = "https://api.picsart.io/tools/demo/adjust"
+        payload={"contrast": "100", "brightness": "0", "saturation": "0", "sharpen": "100", "shadows": "0", "clarity": "100", "temperature": "0", "format": "PNG", "hue": "0", "vignette": "0", "noise": "0", "highlights": "0"}
+        files=[('image',(png_file, open(png_file, 'rb'),'image/png'))]
+        headers = {"accept": "application/json", "apikey": "sifH0Y0MoRLcboeYh899RiWFw29vt0Pz"}
+        response = requests.request("POST", url, headers=headers, data=payload, files=files)
+        data = json.loads(response.text)
+        url = data['data']['url']
+        img_data = requests.get(url).content
+        with open(png_file, 'wb') as handler:
+            handler.write(img_data)
+    
+    # store final result
     png = Image.open(png_file).convert("RGB")
     images.append(png)
 
+# merge resultant pngs into result pdf
 images[0].convert("RGB").save(new_pdf_file, resolution=100.0, save_all=True, append_images=images[1:])
 
+# remove temporary png files
+files_to_remove = f"{args.pdf_file[:-4]}_page*"
+os.system(f"rm {files_to_remove}")
