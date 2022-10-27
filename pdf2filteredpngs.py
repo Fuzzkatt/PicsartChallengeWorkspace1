d6ba491eaeeb53a41ea9e75557a9cf78ba5bbe18
Fuzzkatt <zonghan2000@gmail.com>
2022-10-26 17:05:33 -0700

    finalized

--- pdf2filteredpngs.py
+++ pdf2filteredpngs.py
@@ -16,8 +16,7 @@ images = []
 new_pdf_file = f"{args.pdf_file[:-4]}_fixed.pdf"
 
 # for each page in the pdf
-for i in range(2, 3):
-# for i in range(len(doc)):
+for i in range(len(doc)):
     print(f"working on page {i+1} of {len(doc)}...")
     
     # store page as temporary png files
@@ -25,6 +24,18 @@ for i in range(2, 3):
     pix = page.get_pixmap(dpi=288)
     png_file = f"{args.pdf_file[:-4]}_page{i+1}.png"
     pix.save(png_file)
+
+    # do mandatory upscale
+    url = "https://api.picsart.io/tools/demo/upscale/enhance"
+    payload={"unit": "px", "format": "PNG", "upscale_factor": "2"}
+    files=[('image',(png_file, open(png_file, 'rb'),'image/png'))]
+    headers = {"accept": "application/json", "apikey": "sifH0Y0MoRLcboeYh899RiWFw29vt0Pz"}
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    data = json.loads(response.text)
+    url = data['data']['url']
+    img_data = requests.get(url).content
+    with open(png_file, 'wb') as handler:
+        handler.write(img_data)
 
     # do optional adjusts
     if args.adjust:
@@ -39,18 +50,6 @@ for i in range(2, 3):
         with open(png_file, 'wb') as handler:
             handler.write(img_data)
     
-    # do mandatory upscale
-    url = "https://api.picsart.io/tools/demo/upscale/enhance"
-    payload={"unit": "px", "format": "PNG", "upscale_factor": "2"}
-    files=[('image',(png_file, open(png_file, 'rb'),'image/png'))]
-    headers = {"accept": "application/json", "apikey": "sifH0Y0MoRLcboeYh899RiWFw29vt0Pz"}
-    response = requests.request("POST", url, headers=headers, data=payload, files=files)
-    data = json.loads(response.text)
-    url = data['data']['url']
-    img_data = requests.get(url).content
-    with open(png_file, 'wb') as handler:
-        handler.write(img_data)
-
     # store final result
     png = Image.open(png_file).convert("RGB")
     images.append(png)
